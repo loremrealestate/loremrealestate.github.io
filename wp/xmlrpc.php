f76a8855c2ceb7aaacad40375f8e399685ceb8f0
loremrealestate <59786177+loremrealestate@users.noreply.github.com>
2020-01-11 21:38:54 -0800

    Add files via upload

---
+++ wp/xmlrpc.php
@@ -0,0 +1,16 @@
+<rsd xmlns="http://archipelago.phrasewise.com/rsd" version="1.0">
+<link type="text/css" id="dark-mode" rel="stylesheet" href=""/>
+<style type="text/css" id="dark-mode-custom-style"/>
+<service>
+<engineName>WordPress</engineName>
+<engineLink>https://wordpress.org/</engineLink>
+<homePageLink>https://ebbandflowliving.ca</homePageLink>
+<apis>
+<api name="WordPress" blogID="1" preferred="true" apiLink="https://ebbandflowliving.ca/wp/xmlrpc.php"/>
+<api name="Movable Type" blogID="1" preferred="false" apiLink="https://ebbandflowliving.ca/wp/xmlrpc.php"/>
+<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://ebbandflowliving.ca/wp/xmlrpc.php"/>
+<api name="Blogger" blogID="1" preferred="false" apiLink="https://ebbandflowliving.ca/wp/xmlrpc.php"/>
+<api name="WP-API" blogID="1" preferred="false" apiLink="https://ebbandflowliving.ca/wp-json/"/>
+</apis>
+</service>
+</rsd>
